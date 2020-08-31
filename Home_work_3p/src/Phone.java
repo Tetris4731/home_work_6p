d6f7ada367d3e433a50336cb512b3b4b6f79429f
Tetris4731 <baulema.4731@mail.ru>
2020-08-31 15:25:26 +0300

    add

--- Home_work_3p/src/Phone.java
+++ Home_work_3p/src/Phone.java
@@ -5,21 +5,4 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Phone {
-    private HashMap<String, Set<String>> phoneBook = new HashMap<>();
-
-    public void add(String name, String phone){
-        Set<String> tell = phoneBook.getOrDefault(name,new HashSet<>());
-        tell.add(phone);
-        phoneBook.put(name,tell);
-    }
-
-    public void add (String name, String...phones){
-        Set<String> tell = phoneBook.getOrDefault(name,new HashSet<>());
-        tell.addAll(Arrays.asList(phones));
-        phoneBook.put(name,tell);
-    }
-
-    public Set<String> get(String name){
-        return phoneBook.get(name);
-    }
 }
