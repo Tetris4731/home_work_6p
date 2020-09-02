b8daabe28965eee58a1ec41797eecf8464ed7103
Tetris4731 <baulema.4731@mail.ru>
2020-09-02 16:52:09 +0300

    hw_5p

---
+++ Home_work_3p/src/Test.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test {
+//    import java.lang.reflect.Array;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//    public class Phone {
+//        private HashMap<String, Set<String>> phoneBook = new HashMap<>();
+//
+//        public void add(String name, String phone){
+//            Set<String> tell = phoneBook.getOrDefault(name,new HashSet<>());
+//            tell.add(phone);
+//            phoneBook.put(name,tell);
+//        }
+//
+//        public void add (String name, String...phones){
+//            Set<String> tell = phoneBook.getOrDefault(name,new HashSet<>());
+//            tell.addAll(Arrays.asList(phones));
+//            phoneBook.put(name,tell);
+//        }
+//
+//        public Set<String> get(String name){
+//            return phoneBook.get(name);
+//        }
+//    }
+//public static void main( String[] args ) {
+//    String[] words = {"Deushev","Ivanov","Kozlov","Deushev","Ivanov","Kozlov","Deushev","Ivanov","Kozlov"};
+//    System.out.println(Arrays.toString(words));
+//
+//    HashMap<String,Integer> hmap = new HashMap<>();
+//    for (String w : words) {
+//        hmap.put(w,hmap.getOrDefault(w,0)+1);
+//    }
+//    System.out.println(hmap);
+//
+//
+//    Phone phBook = new Phone();
+//    phBook.add("Deushev","8(917)033-48-50");
+//    phBook.add("Deushev","8(917)000-48-50");
+//    phBook.add("Ivanov","8(917)333-48-50");
+//    phBook.add("Ivanov","8(917)033-88-50");
+//    phBook.add("Kozlov","8(917)033-48-51");
+//    phBook.add("Kozlov","8(917)033-33-50");
+//    System.out.println("Phone " + phBook.get("Kozlov"));
+//}
+}
