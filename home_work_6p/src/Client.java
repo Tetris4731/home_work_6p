a93612c98d75706c87504c04f23118c5ac86939e
Tetris4731 <68471468+Tetris4731@users.noreply.github.com>
2020-09-10 17:03:17 +0300

    Add files via upload

--- home_work_6p/src/Client.java
+++ home_work_6p/src/Client.java
@@ -1,2 +1,55 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    private static final String server = "localhost";
+    private static final int port = 60080;
+
+    public static void main( String[] args ) {
+        Socket socket = null;
+        Scanner scanner = new Scanner( System.in );
+        try {
+            socket = new Socket(server, port );
+            System.out.println("Client joying" + socket.getRemoteSocketAddress());
+            DataInputStream inputStream = new DataInputStream( socket.getInputStream() );
+            DataOutputStream outputStream = new DataOutputStream( socket.getOutputStream() );
+
+            Thread threadReader = new Thread (() ->{
+                while (true){
+                    try {
+                        outputStream.writeUTF( scanner.nextLine() );
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            threadReader.setDaemon( true );
+            threadReader.start();
+
+            while(true){
+                String str = inputStream.readUTF();
+                if(str.equals( "quite" )){
+                    System.out.println("Lost connection to server");
+                    outputStream.writeUTF( "quite" );
+                    break;
+                }else {
+                    System.out.println("Server " + str);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            try {
+                socket.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
 }
+
+
