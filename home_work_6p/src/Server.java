15d9d6af359000046ef754abc1bd589c117e9b80
Tetris4731 <68471468+Tetris4731@users.noreply.github.com>
2020-09-12 19:33:34 +0300

    Add files via upload

--- home_work_6p/src/Server.java
+++ home_work_6p/src/Server.java
@@ -1,2 +1,77 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Vector;
+
 public class Server {
+    private List<ClientHandler> clients;
+    private AuthService authService;
+
+    private int PORT = 8189;
+    ServerSocket server = null;
+    Socket socket = null;
+
+    public Server(){
+        clients = new Vector<>();
+        authService = new SimpleAuthService();
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Сервер запущен");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+
+                new ClientHandler(this, socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public void broadcastMsg(ClientHandler sender, String msg){
+        String message = String.format("%s : %s", sender.getNickname(), msg);
+        for (ClientHandler c : clients) {
+            c.sendMsg(message);
+        }
+    }
+
+    public void privateMsg(ClientHandler sender,String receiver, String msg){
+        String message = String.format("[%s] privet [%s] : %s", sender.getNickname(),receiver, msg);
+        for (ClientHandler c : clients) {
+            if(c.getNickname().equals( receiver )){
+                c.sendMsg(message);
+                if(!c.equals( sender )){
+                    sender.sendMsg( message );
+                }
+                return;
+            }
+        }
+        sender.sendMsg( "user is not found: " + receiver );
+    }
+
+
+    public void subscribe(ClientHandler clientHandler){
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler){
+        clients.remove(clientHandler);
+    }
+
 }
